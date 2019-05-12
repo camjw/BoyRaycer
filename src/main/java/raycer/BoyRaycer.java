f12f4f4772cc940ca2aa21edc4809ad94073b156
camjw <camjw119@gmail.com>
2019-05-12 13:25:13 +0100

    Stll cant get it to compile

--- src/main/java/raycer/BoyRaycer.java
+++ src/main/java/raycer/BoyRaycer.java
@@ -1,8 +1,29 @@
 package raycer;
 
+import java.util.ArrayList;
+
 public class BoyRaycer {
+	
+	static int width = 1024;
+	static int height = 768;
+	
 	public static void main(String[] args) {
-		System.out.println("Did this work?");
+		render();
+	}
+
+	public static void render() {
+
+		ArrayList<ArrayList<Float>> framebuffer = new ArrayList<ArrayList<Float>>();
+
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
+				float red = j/((float) height);
+				float green =  i/((float) width);
+				ArrayList<Float> currentPixel = new ArrayList<Float>(3);
+				
+				framebuffer.set(i * j * width, currentPixel);
+			}
+		}
 	}
 }
 
